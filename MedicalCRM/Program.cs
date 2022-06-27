0d6b117a67087e5002f42db27b4156ca9a8bda10
SyrdybaevAidar <72427980+SyrdybaevAidar@users.noreply.github.com>
2022-06-28 01:29:30 +0600

    test

--- MedicalCRM/Program.cs
+++ MedicalCRM/Program.cs
@@ -18,13 +18,8 @@ builder.Services.AddSignalR();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
-    app.UseMigrationsEndPoint();
-} else {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+
+app.UseMigrationsEndPoint();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
