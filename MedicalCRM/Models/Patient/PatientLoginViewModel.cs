1234bbfae79f653f6d49d6b145375d418ae7bedb
SyrdybaevAidar <72427980+SyrdybaevAidar@users.noreply.github.com>
2022-07-03 12:51:26 +0600

    test

--- MedicalCRM/Models/Patient/PatientLoginViewModel.cs
+++ MedicalCRM/Models/Patient/PatientLoginViewModel.cs
@@ -1,6 +1,11 @@
-﻿namespace MedicalCRM.Models.Patient {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedicalCRM.Models.Patient {
     public class PatientLoginViewModel {
+        [Display(Name = "Инн")]
+        [Required]
         public string Inn { get; set; }
+        [Display(Name = "Запомнить меня")]
         public bool RememberMe { get; set; }
     }
 }
